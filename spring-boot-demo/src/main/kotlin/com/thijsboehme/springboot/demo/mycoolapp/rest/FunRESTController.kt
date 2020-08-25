4028d4adaeaa8329a3d7bec7e3efb727368bce00
Thijs Boehme <thijs.boehme@qmino.com>
2020-08-25 15:57:54 +0200

    Section 73: Spring Boot - Application Properties

--- spring-boot-demo/src/main/kotlin/com/thijsboehme/springboot/demo/mycoolapp/rest/FunRESTController.kt
+++ spring-boot-demo/src/main/kotlin/com/thijsboehme/springboot/demo/mycoolapp/rest/FunRESTController.kt
@@ -1,11 +1,19 @@
 package com.thijsboehme.springboot.demo.mycoolapp.rest
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 
 @RestController
-class FunRESTController {
+class FunRESTController(
+    // Inject properties for coach.name and team.name
+    @Value("\${coach.name}")
+    private val coachName: String,
+
+    @Value("\${team.name}")
+    private val teamName: String
+) {
     @GetMapping("/")
     fun sayHello(): String {
         return "Hello World! Time on the server is ${LocalDateTime.now()}"
@@ -19,5 +27,10 @@ class FunRESTController {
     @GetMapping("/fortune")
     fun getFortune(): String {
         return "Today is your lucky day!"
+    }
+
+    @GetMapping("/teaminfo")
+    fun getTeamInfo(): String {
+        return "Coach: $coachName, Team: $teamName"
     }
 }
